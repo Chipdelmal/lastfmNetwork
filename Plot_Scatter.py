e23329f51cc939945400dc93ebac9fa7657cf35e
chipdelmal <chipdelmal@gmail.com>
2022-07-04 09:16:49 -0700

    Added full data process pipeline

--- Plot_Scatter.py
+++ Plot_Scatter.py
@@ -16,7 +16,7 @@ import markov as mkv
 
 #if aux.isnotebook():
 (USERNAME, PTH_DTA, PTH_CHE, PTH_IMG, TOP, WRAN) = (
-    'chipmaligno', './data', './cache', './img', 300, 3
+    'chipmaligno', './data', './cache', './img', 250, 3
 )
 # else:
 #     (USERNAME, PTH_DTA, PTH_CHE, PTH_IMG, TOP, WRAN) = (
@@ -64,18 +64,18 @@ for (ix, art) in enumerate(artists):
 ###############################################################################
 # Plot Scatter
 ###############################################################################
-norm = colors.LogNorm(vmin=1, vmax=50)
+norm = colors.LogNorm(vmin=1, vmax=40)
 # norm = colors.Normalize(vmin=0, vmax=50) # np.max(countsArray))
 # Iterate through artists -----------------------------------------------------
 pad = 1
 artPad = 5
 cYear = to.year
-(fig, ax) = plt.subplots(figsize=(6, 10))
+(fig, ax) = plt.subplots(figsize=(19.2/2, 10.8/2), dpi=1500) #plt.subplots(figsize=(6, 10))
 for (r, art) in enumerate(artists):
     clr = cst.MAPS[r%len(cst.MAPS)]
     for day in np.nonzero(countsArray[r])[0]:
         plt.plot(
-            [r-.225, r+.225], [day, day],
+            [r-.325, r+.325], [day, day],
             color=clr(norm(countsArray[r][day])),
             lw=.2, zorder=5
         )
@@ -83,14 +83,34 @@ for d in range(len(countsArray[0])):
     (tod, tfd) = (d, 0)
     if (to+timedelta(int(d))).year > cYear:
         plt.hlines(
-            d, -pad, len(artists)+pad, '#ffffff44', 
-            lw=.1, ls='--', zorder=-10
+            d, -pad*100, -pad, '#ffffffBB', 
+            lw=.35, ls='-', zorder=-10
         )
+        plt.hlines(
+            d, len(artists)+pad, len(artists)+100*pad, '#ffffffbb', 
+            lw=.35, ls='-', zorder=-10
+        )
+        # plt.hlines(
+        #     d, -pad, len(artists)+pad, '#ffffff22', 
+        #     lw=.2, ls='-', zorder=-10
+        # )
+        # ax.axhspan(
+        #     d+5, d+365-5, -pad, len(artists)+pad, 
+        #     fc = '#ffffff11', zorder=-10
+        #     # lw=.2, ls='-', zorder=-10 
+        # )
         cYear += 1
         if cYear < tf.year:
             ax.text(
                 -2, (d-tod)/2+tod+365/2, cYear, fontsize=4.5, 
-                color='#ffffff66', 
+                color='#ffffff', 
+                horizontalalignment='right',
+                verticalalignment='center',
+                rotation=90
+            )
+            ax.text(
+                len(artists)+2, (d-tod)/2+tod+365/2, cYear, fontsize=4.5, 
+                color='#ffffff', 
                 horizontalalignment='right',
                 verticalalignment='center',
                 rotation=90
@@ -99,27 +119,27 @@ for d in range(len(countsArray[0])):
 for (ix, a) in enumerate(artists):
     ax.text(
         ix, -artPad, a, 
-        fontsize=.1, color='w', 
+        fontsize=1.75, color='w', 
         horizontalalignment='center',
         verticalalignment='top',
         rotation=90
     )
     ax.text(
         ix, len(countsArray[0])+4*artPad, a, 
-        fontsize=.1, color='w', 
+        fontsize=1.75, color='w', 
         horizontalalignment='center',
         verticalalignment='bottom',
         rotation=90
     )
-ax.set_aspect(.5/ax.get_data_ratio())
+# ax.set_aspect(.25/ax.get_data_ratio())
 ax.set_facecolor('#000000')
 ax.set_xlim(-pad*2, len(artists)+2*pad)
 ax.set_ylim(-pad, daysTotal+pad)
 ax.axis('off')
 fName = 'Scatter.png'
 plt.savefig(
-    path.join(PTH_IMG, fName),
-    dpi=1500, transparent=True, facecolor='#000000', 
+    path.join(PTH_IMG, fName), # dpi=1500, 
+    transparent=True, facecolor='#000000', 
     bbox_inches='tight'
 )
 plt.close('all')
