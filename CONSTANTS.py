87e7db43ded72faade0d39b4cce8bc2b28a7184e
chipdelmal <chipdelmal@gmail.com>
2022-07-04 21:03:23 -0700

    Increasing reps max to 150

--- CONSTANTS.py
+++ CONSTANTS.py
@@ -6,7 +6,7 @@ import auxiliary as aux
 from datetime import date, timedelta
 
 TIMEZONE = 'US/Pacific'
-DAILY_PLAYCOUNT_LIMIT = 100
+DAILY_PLAYCOUNT_LIMIT = 150
 PLAYTIME_THRESHOLD = timedelta(minutes=30)
 DF_DTE_RANGE = (date(2010, 1, 1), date(2050, 1, 1))
 ##############################################################################
