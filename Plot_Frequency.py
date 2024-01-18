8b82c2f9e079cd750c7a346efd2e5d889fd15664
chipdelmal <chipdelmal@gmail.com>
2024-01-18 12:58:45 -0800

    BumpChart Artist highlight implemented now

--- Plot_Frequency.py
+++ Plot_Frequency.py
@@ -21,8 +21,9 @@ else:
     (USERNAME, PTH_DTA, PTH_CHE, PTH_IMG, TOP, WRAN) = (
         argv[1], argv[2], argv[3], argv[4], int(argv[5]), int(argv[6])
     ) 
-TOP_ARTISTS = 12
+TOP_ARTISTS = 10
 SORTED = True
+HIGHLIGHT = 'Wilco' # None
 ###############################################################################
 # Read Data
 ###############################################################################
@@ -69,15 +70,14 @@ A_TOP[A_TOP['Artist'].isin(names)]
 ###############################################################################
 # Generate Ranking
 ###############################################################################
-DTA_RNK = DTA_CNT.rank(axis=0, ascending=False, method='min').astype(int)
-DTA_RNK_FULL = DTA_RNK.loc[list(A_TOP['Artist'][:TOP_ARTISTS])]
-
-
-DTA_RNK_TOP = DTA_CNT.loc[list(A_TOP['Artist'][:TOP_ARTISTS])].rank(
+# DTA_RNK = DTA_CNT.rank(axis=0, ascending=False, method='min').astype(int)
+# DTA_RNK_FULL = DTA_RNK.loc[list(A_TOP['Artist'][:TOP_ARTISTS])]
+artistsList = list(A_TOP['Artist'][:TOP_ARTISTS])
+DTA_RNK_TOP = DTA_CNT.loc[artistsList].rank(
     axis=0, ascending=False, method='first'
 ).astype(int)
 DTA_RNK_TOP = DTA_RNK_TOP.sort_values(by=DTA_RNK_TOP.columns[0])
-DTA_RNK_TOP.loc['Wilco']
+# DTA_RNK_TOP.loc['Wilco']
 ###############################################################################
 # Plot
 ###############################################################################
@@ -86,6 +86,7 @@ COLS = [
     '#F5E4BF', '#DFA145', '#CC8D6F', '#D1C87A',
     '#D3B1C1', '#9FC4E5', '#FFC41A', '#FCC176', '#A1BE7C'
 ]
+FONT_SIZE = 14
 
 (yInts, arts) = (
     [int(i.year) for i in list(DTA_RNK_TOP.columns)],
@@ -97,26 +98,42 @@ artUnsorted = list(DTA_RNK_TOP.index)
     DTA_RNK_TOP[DTA_RNK_TOP.columns[0]].values,
     DTA_RNK_TOP[DTA_RNK_TOP.columns[-1]].values
 )
+(lblL, lblR) = (
+    [x for _,x in sorted(zip(artSortStart, artUnsorted))[::-1]],
+    [x for _,x in sorted(zip(artSortEnd, artUnsorted))[::-1]]
+)
 
-(fig, ax) = plt.subplots(figsize=(15, 4))
+(fig, ax) = plt.subplots(figsize=(15, 5))
 for (ix, art) in enumerate(arts[:]):
+    if art==HIGHLIGHT:
+        (color, zorder) = ('#000000', 10)
+    else:
+        (color, zorder) = (COLS[ix%len(COLS)], 1)
     ax.plot(
         yInts, 
         [TOP_ARTISTS-i for i in list(DTA_RNK_TOP.loc[art])],
-        c=COLS[ix%len(COLS)], lw=5,
-        zorder=1
+        c=color, lw=5,
+        zorder=zorder, alpha=0.75
     )
     ax.scatter(
         yInts, 
         [TOP_ARTISTS-i for i in list(DTA_RNK_TOP.loc[art])], 
-        c='w', ec=COLS[ix%len(COLS)], alpha=1, s=75, lw=2,
-        zorder=10
+        c='w', ec=color, alpha=1, s=75, lw=2,
+        zorder=zorder
     )
-ax.set_xlim(min(yInts), max(yInts))
+ax.set_xticks(yInts)
+ax.set_xticklabels(yInts, size=FONT_SIZE, rotation=0)
+ax.set_xlim(min(yInts)-.125, max(yInts)+.125)
 ticks = range(0, TOP_ARTISTS)
 ax.set_yticks(range(0, TOP_ARTISTS))
-ax.set_yticklabels([x for _,x in sorted(zip(artSortStart, artUnsorted))[::-1]])
+ax.set_yticklabels(lblL, size=FONT_SIZE)
+ax.tick_params(axis='both', which='both',length=0)
 ax2 = ax.twinx()
+ax2.tick_params(axis='both', which='both',length=0)
 ax2.set_ylim([0, TOP_ARTISTS])
 ax2.set_yticks(np.arange(0.5, TOP_ARTISTS+0.5))
-ax2.set_yticklabels([x for _,x in sorted(zip(artSortEnd, artUnsorted))[::-1]])
+ax2.set_yticklabels(lblR, size=FONT_SIZE)
+for x in (ax, ax2):
+    [x.spines[i].set_visible(False) for i in ('top', 'right', 'bottom', 'left')]
+[ax.axvline(i, 0, 1, ls=':', color='#58586B', zorder=-1) for i in yInts]
+# [ax.axhline(i, 0, 1, ls=':', color='#58586B') for i in range(0, TOP_ARTISTS, 5)]
